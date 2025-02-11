488bdf370ca689422a57397deab374215529ad54
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2025-02-11 02:55:39 -0600

    Se creo el 5% del proyecto

---
+++ configs/server.js
@@ -0,0 +1,33 @@
+"use strict"
+
+import express from 'express'
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import { dbConnection } from './mongo.js'
+
+const configs = (app) =>{
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+}
+
+const ConnectarDB = async() =>{
+    try{
+        await dbConnection()
+    }catch(err){
+        console.log(`Database connection failed: ${err}`)
+    }
+}
+
+export const initServer = () => {
+    const app = express()
+    try{
+        configs(app)
+        ConnectarDB()
+        app.listen(process.env.PORT)
+        console.log(`Server running on port: ${process.env.PORT}`)
+    }catch(err){
+        console.log(`Server init failed: ${err}`)
+    }
+}
