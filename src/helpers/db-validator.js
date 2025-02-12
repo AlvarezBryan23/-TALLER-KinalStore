51f3b6dfd82f03441f81feba673edce0a1c06083
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2025-02-12 14:30:18 -0600

    Se agrego el login para usuarios

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -6,3 +6,17 @@ export const existeEmail = async(email = '') =>{
         throw new Error(`El email ${email} ya fue registrado previamente`)
     }
 }
+
+export const existeUsername = async(username = '') =>{
+    const existe = await User.findOne({username})
+    if(existe){
+        throw new Error(`El usuario ${username} ya fue registrado previamente`)
+    }
+}
+
+export const userExits = async(uid = '') =>{
+    const existe = await User.findById(uid)
+    if(!existe){
+        throw new Error("El usuario no existe")
+    }
+}
