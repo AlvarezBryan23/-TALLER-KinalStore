6822a09a4dce37e5dbac1fa5664028f8bf34d12e
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2025-03-03 23:15:36 -0600

    Se creo el listar

--- src/categoria/categoria.controller.js
+++ src/categoria/categoria.controller.js
@@ -1,7 +1,4 @@
-
-
 import Categoria from "../categoria/categoria.model.js"
-
 
 export const saveCategoria = async(req, res) =>{
     try{
@@ -14,13 +11,39 @@ export const saveCategoria = async(req, res) =>{
         await categoria.save();
 
         res.status(200).json({
-            success: true,
+            success: true, 
             categoria
         }) 
     }catch(err){
         res.status(500).json({
             success: false,
             message: "Error al guardar una categoria",
+            error: err.message
+        })
+    }
+}
+
+export const listCategoria = async(req, res) =>{
+    try{
+        const { limite = 11, desde = 0 } = req.query
+        const query = {status: true}
+
+        const [total, categoria ] = await Promise.all([
+            Categoria.countDocuments(query),
+            Categoria.find(query)
+                     .skip(Number(desde))
+                     .limit(Number(limite))
+        ])
+
+        return res.status(200).json({
+            success: true,
+            total,
+            categoria
+        })
+    }catch(err){
+        res.status(500).json({
+            success: false,
+            message: "Error al obtener las categorias",
             error: err.message
         })
     }
--- src/categoria/categoria.route.js
+++ src/categoria/categoria.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { saveCategoriaValidator } from "../middlewares/categoria-validator.js";
-import { saveCategoria } from "../categoria/categoria.controller.js";
+import { saveCategoria, listCategoria } from "../categoria/categoria.controller.js";
 
 const router = Router();
 
 router.post("/addCategoria", saveCategoriaValidator, saveCategoria);
+
+router.get("/", listCategoria)
 
 export default router;
